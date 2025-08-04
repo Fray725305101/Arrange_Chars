7b3a02728641b59f831bddcf7c7f7b3aed2fc030
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-08-04 21:54:45 +0300

    Commit

--- src/Arrange_Chars/Main.java
+++ src/Arrange_Chars/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter symbols: ");
-        String imputString = scanner.nextLine();
+        String inputString = scanner.nextLine();
+        String[] splitInputString = inputString.split(" ", 2); //Разделитель пробел, 2 части
     }
 }
