6480a140c0dcf88aed8f483aaf810e520d38c53c
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-08-04 22:08:43 +0300

    Commit

--- src/Arrange_Chars/Main.java
+++ src/Arrange_Chars/Main.java
@@ -1,12 +1,25 @@
 package Arrange_Chars;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter symbols: ");
+        System.out.print("Enter symbols: ");
         String inputString = scanner.nextLine();
         String[] splitInputString = inputString.split(" ", 2); //Разделитель пробел, 2 части
+        String leftPart = Main.arrange(splitInputString[0]);
+        String rightPart = Main.arrange(splitInputString[1]);
+        System.out.println(leftPart+" "+rightPart);
+    }
+
+    public static String arrange (String inputSubstr) {
+        //Приводим к нижнему регистру и сразу преобразуем строку в массив символов
+        //т.к. строки в Java неизменны:
+        char[] inputSubstrChar = inputSubstr.toLowerCase().toCharArray();
+        Arrays.sort(inputSubstrChar); //Сортируем массив символов
+        String sortedString = new String(inputSubstrChar); //Собираем отсортированные символы обратно в строку
+        return sortedString;
     }
 }
